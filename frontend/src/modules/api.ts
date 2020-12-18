ec8539236fe13e3130cc6a31d1100f3d273e50f5
André Lergier <andre@lergier.ch>
2020-12-18 18:18:02 +0100

    update api link

--- frontend/src/modules/api.ts
+++ frontend/src/modules/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4433' : 'https://hslu-code-project-collection.herokuapp.com/',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4433' : 'https://hslu-code-project-collection.herokuapp.com',
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
 });
